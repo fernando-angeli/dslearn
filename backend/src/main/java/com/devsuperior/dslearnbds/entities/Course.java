bcd6b65e58f688d796618a542557e2706c7b90b9
fernando-angeli <fernando_angeli@hotmail.com>
2022-08-09 15:39:06 -0300

    Course

---
+++ backend/src/main/java/com/devsuperior/dslearnbds/entities/Course.java
@@ -0,0 +1,40 @@
+package com.devsuperior.dslearnbds.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_course")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Course implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String imgUri;
+    private String imgGrayUri;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Course course = (Course) o;
+        return id.equals(course.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -2,7 +2,6 @@ INSERT INTO tb_user (name, email, password) VALUES ('Alex Brown', 'alex@gmail.co
 INSERT INTO tb_user (name, email, password) VALUES ('Bob Green', 'bob@gmail.com', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG');
 INSERT INTO tb_user (name, email, password) VALUES ('Maria Green', 'maria@gmail.com', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG');
 
-
 INSERT INTO tb_role (authority) VALUES ('ROLE_STUDENT');
 INSERT INTO tb_role (authority) VALUES ('ROLE_INSTRUCTOR');
 INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
@@ -13,3 +12,5 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
+
+INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) VALUES ('Bootcamp HTML', 'https://scholarship-positions.com/wp-content/uploads/2016/10/Free-Online-Course.jpg','https://cdn-icons-png.flaticon.com/512/5351/5351078.png');
