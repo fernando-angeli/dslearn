a9546a140d85f9f6612cee67dfd2925a4ef6e9cb
fernando-angeli <fernando_angeli@hotmail.com>
2022-08-12 15:41:06 -0300

    Exceptions, validation and security

---
+++ backend/src/main/java/com/devsuperior/dslearnbds/controllers/exceptions/ValidationError.java
@@ -0,0 +1,23 @@
+package com.devsuperior.dslearnbds.controllers.exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ValidationError extends StandardError{
+    private static final long serialVersionUID = 1L;
+
+    private List<FieldMessage> errors = new ArrayList<>();
+
+    public void addError(String fieldName, String message){
+        errors.add(new FieldMessage(fieldName, message));
+    }
+}
